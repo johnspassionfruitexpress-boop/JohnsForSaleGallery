5bcbd02f7f717873692b484f97084221644685d3
johnspassionfruitexpress-boop <johnspassionfruitexpress@gmail.com>
2026-04-26 19:49:53 -0400

    Delete netlify-site/netlify/functions/netlify-site/netlify/functions directory

--- netlify-site/netlify/functions/netlify-site/netlify/functions/list-listings.mjs
+++
@@ -1,7 +0,0 @@
-import { getStore } from "@netlify/blobs";
-
-export default async () => {
-  const store = getStore("jpfe-listings");
-  const listings = (await store.get("items", { type: "json" })) || [];
-  return Response.json(listings);
-};
